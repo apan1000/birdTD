2f7bdad27fbf53085891045f8a385f8d8c2413dd
Fredrik Berglund <fredrik.911225@gmail.com>
2020-06-08 15:22:53 +0200

    Add tiles

--- lib/bird-td-game.dart
+++ lib/bird-td-game.dart
@@ -1,23 +1,35 @@
 import 'dart:ui';
-import 'package:flame/gestures.dart';
 
+import 'package:birdTD/tile-map.dart';
 import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
 import 'package:flutter/cupertino.dart';
 
 class BirdTDGame extends Game with TapDetector {
-
   Size screenSize;
+  TileMap tiles = TileMap(
+      [1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1]
+          .map((e) => TileType.values[e])
+          .toList(),
+      3);
+
+  Paint paint = Paint();
 
   void render(Canvas canvas) {
-    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint bgPaint = Paint();
-    bgPaint.color = Color(0xff55aa33);
-    canvas.drawRect(bgRect, bgPaint);
+    var screenWidth = screenSize.width;
+    var screenHeight = screenSize.height;
+    for (int x = 0; x < tiles.width; x++) {
+      for (int y = 0; y < tiles.height; y++) {
+        var width = screenWidth / tiles.width;
+        var height = screenHeight / tiles.height;
+        Rect rect = Rect.fromLTWH(x * width, y * height, width, height);
+        paint.color = getTileColor(tiles.get(x, y));
+        canvas.drawRect(rect, paint);
+      }
+    }
   }
 
-  void update(double t) {
-
-  }
+  void update(double t) {}
 
   void resize(Size size) {
     screenSize = size;
@@ -25,7 +37,8 @@ class BirdTDGame extends Game with TapDetector {
   }
 
   void onTapDown(TapDownDetails details) {
-    print("Tap down: ${details.globalPosition.dx}, ${details.globalPosition.dy}");
+    print(
+        "Tap down: ${details.globalPosition.dx}, ${details.globalPosition.dy}");
   }
 
   void onTapUp(TapUpDetails details) {
@@ -34,5 +47,15 @@ class BirdTDGame extends Game with TapDetector {
 
   void onTapCancel() {
     print("Tap cancel");
+  }
+
+  Color getTileColor(TileType tileType) {
+    switch (tileType) {
+      case TileType.dirt:
+        return Color(0xff886633);
+      case TileType.grass:
+        return Color(0xff558833);
+    }
+    return Color(0xff000000);
   }
 }
---
+++ lib/tile-map.dart
@@ -0,0 +1,27 @@
+class TileMap {
+  final List<TileType> tiles;
+  final int width;
+  final int height;
+
+  TileMap(this.tiles, this.width) : height = tiles.length ~/ width;
+}
+
+enum TileType { dirt, grass }
+
+extension TileMapExtensions on TileMap {
+  int size() {
+    return tiles.length;
+  }
+
+  void forEach(void Function(TileType) f) {
+    tiles.forEach(f);
+  }
+
+  void forEachIndexed(void Function(int, TileType) f) {
+    tiles.asMap().forEach(f);
+  }
+
+  TileType get(int x, int y) {
+    return tiles[x + y * width];
+  }
+}
